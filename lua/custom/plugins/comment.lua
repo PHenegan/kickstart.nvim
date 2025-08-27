29be821a4d28739bfff19f910e7ba2b2bd5d0bab
Patrick Henegan <phenegan03@gmail.com>
2025-08-27 09:47:24 -0400

    feat(plugins): add undotree and comment plugins

---
+++ lua/custom/plugins/comment.lua
@@ -0,0 +1,20 @@
+return {
+    'numToStr/Comment.nvim',
+    opts = {
+        toggler = {
+            line = "<leader>cc",
+            block = "<leader>bcc"
+        },
+        -- for visual mode
+        opleader = {
+            line = "<leader>cc",
+            block = "<leader>bcc"
+        },
+        -- adding comments around a line
+        extra = {
+            above = "<leader>ck",
+            below = "<leader>cj",
+            eol = "<leader>cl"
+        }
+    }
+}
